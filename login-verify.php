de7df148d21dbabc26f09e3e752b9fdc8364c438
Fiona <fionajovita97@gmail.com>
2018-11-09 09:46:18 +0530

    Login Connection Complete

--- login-verify.php
+++ login-verify.php
@@ -3,18 +3,23 @@
     $db_username = "root";
     $password = "Difi3540";
     $dbname = "EventManagement";
-
     // Create connection
     $conn = new mysqli($servername, $db_username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }   
-
     $Username = $_POST['Username'];
     $Password = $_POST['Password'];
-     
-    
+    $sql = "SELECT Username, Password FROM User WHERE Username = '$Username' AND Password = '$Password'";
+    $query = mysqli_query($conn, $sql);
+    $numrows = mysqli_num_rows($query);
+    if ($numrows!=0)
+    {
+        echo "<script> window.location.assign('venue.php'); </script>";
+    }
+    else{
+        echo "<script> window.location.assign('login.html'); alert('Invalid Login Credentials!'); </script>";
+    } 
     $conn->close();
-    
 ?>
---
+++ venue.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Venue</title>
+        <link href="css/venue.css" rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <div class = "venue-container">
+            <div class = "single-ven">
+                
+            </div>  
+        </div>
+    </body>
+</html>
+<?php
+    
+?>
